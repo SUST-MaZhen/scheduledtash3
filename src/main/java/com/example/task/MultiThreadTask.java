d1eb5d98560fbef9523e91c1d976dbea7f4ca2a5
sust-mazhen <291540704@qq.com>
2020-06-07 19:07:18 +0800

    Initial commit

---
+++ src/main/java/com/example/task/MultiThreadTask.java
@@ -0,0 +1,34 @@
+package com.example.task;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+/**
+ * @Description
+ * @ClassName MultiThreadTask
+ * @Author User
+ * @date 2020.06.07 18:56
+ */
+@EnableScheduling   // 1.开启定时任务
+@EnableAsync        // 2.开启多线程
+@Component
+public class MultiThreadTask {
+
+    @Async
+    @Scheduled(fixedDelay = 1000)  //间隔1秒
+    public void first() throws InterruptedException {
+        System.out.println("第一个定时任务开始 : " + LocalDateTime.now().toLocalTime() + "\r\n线程 : " + Thread.currentThread().getName());
+        Thread.sleep(1000 * 10);
+    }
+
+    @Async
+    @Scheduled(fixedDelay = 2000)
+    public void second() {
+        System.out.println("第二个定时任务开始 : " + LocalDateTime.now().toLocalTime() + "\r\n线程 : " + Thread.currentThread().getName());
+    }
+}
---
+++ src/test/java/com/example/scheduledtash3/Scheduledtash3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scheduledtash3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Scheduledtash3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
